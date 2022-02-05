244b2431d651241549611b62f3644b0c1184f47c
Edward Guan <edward.guan@mkcorp.com>
2022-02-05 21:23:38 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import Axios from 'axios'
 
+Vue.prototype.$http = Axios				//$[http]:http随便取名
 Vue.config.productionTip = false
 
 new Vue({
